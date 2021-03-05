25cc936c3589a9918adc45dd8ebbeef43fcfc03e
Umut Sever <severumut@yahoo.com>
2021-03-05 17:13:05 +0300

    Initial Commit

---
+++ HaberlerNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HaberlerNews
+//
+//  Created by Umut Sever on 5.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
