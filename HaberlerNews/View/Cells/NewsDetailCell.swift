43ee182cf94042e43356a78f501417840de948fc
Umut Sever <severumut@yahoo.com>
2021-03-06 13:33:30 +0300

    VideoPlayer

---
+++ HaberlerNews/View/Cells/NewsDetailCell.swift
@@ -0,0 +1,25 @@
+//
+//  NewsDetailCell.swift
+//  HaberlerNews
+//
+//  Created by Umut Sever on 6.03.2021.
+//
+
+import UIKit
+
+class NewsDetailCell: UITableViewCell {
+
+    @IBOutlet weak var topTitle: UILabel!
+    @IBOutlet weak var topView: UIView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- HaberlerNews/View/NewsDetailVC.swift
+++ HaberlerNews/View/NewsDetailVC.swift
@@ -14,31 +14,64 @@ import AudioToolbox
 
 class NewsDetailVC: UIViewController, AVPlayerViewControllerDelegate {
     
+    @IBOutlet weak var topView: UIView!
+    @IBOutlet weak var topTitle: UILabel!
+    
+    @IBOutlet weak var tableView: UITableView!
     var theNewsDetail = [NewsTableModel]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print(theNewsDetail)
+        print(theNewsDetail[0].videoUrl)
         
+        playVideo()
+        //        tableView.register(UINib(nibName: "NewsDetailCell", bundle: .main), forCellReuseIdentifier: "newsTopCell")
+        //
+        //        tableView.dataSource = self
         
-     
-      
     }
     
-
+    
     
     
     
     func playVideo() {
         let videoURL = URL(string: theNewsDetail[0].videoUrl)
-            let player = AVPlayer(url: videoURL!)
-            let playervc = AVPlayerViewController()
-            playervc.delegate = self
-            playervc.player = player
-            self.present(playervc, animated: true) {
-                playervc.player!.play()
-            }
-    }
 
+        let asset = AVAsset(url: videoURL!)
+        let playerItem = AVPlayerItem(asset: asset)
+        let player = AVPlayer(playerItem: playerItem)
+        
+        let playerLayer = AVPlayerLayer(player: player)
+     
+        playerLayer.videoGravity = .resizeAspect
+        self.topView.layer.addSublayer(playerLayer)
+        playerLayer.frame = self.topView.bounds
+        
+        player.play()
+        
+    }
+    
+    
+   
+    
 }
+
+
+//extension NewsDetailVC: UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 1
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//
+//
+//
+//
+//       return cell
+//
+//    }
+//
+//
+//}
