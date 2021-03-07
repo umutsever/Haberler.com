3b05d80ecf58305ad032cc69ce4d09859d241b81
Umut Sever <severumut@yahoo.com>
2021-03-07 21:45:11 +0300

    Video Player Landscape

--- HaberlerNews/View/NewsDetailVC.swift
+++ HaberlerNews/View/NewsDetailVC.swift
@@ -15,8 +15,8 @@ import SDWebImage
 
 class NewsDetailVC: UIViewController, AVPlayerViewControllerDelegate {
     
-  
-  
+    
+    
     @IBOutlet weak var tableView: UITableView!
     var theNewsDetail = [NewsTableModel]()
     var newsImage = UIImageView()
@@ -50,67 +50,88 @@ class NewsDetailVC: UIViewController, AVPlayerViewControllerDelegate {
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransition(to: size, with: coordinator)
         
-      
+        
         if UIDevice.current.orientation.isLandscape {
             print("Landscape deyim")
-
             NotificationCenter.default.post(name: NSNotification.Name(rawValue: "landscapeMode"), object: nil)
+            
             
         } else if UIDevice.current.orientation.isPortrait  {
             print("Potrait deyim")
-            NotificationCenter.default.post(name: NSNotification.Name(rawValue: "portraitMode"), object: nil)
-            }
             
-            
+        }
         
+        
+        
+    }
+    
+    func rotate() {
+        NotificationCenter.default.addObserver(self, selector: #selector(landscapeModePlayer), name: NSNotification.Name("landscapeMode"), object: nil)
     }
     
     
     func videoPlaying (bound: CGRect, theLayer: UIView) {
         
+        let bounds = bound
         
         let videoURL = URL(string: theNewsDetail[0].videoUrl)
         
         let asset = AVAsset(url: videoURL!)
         let playerItem = AVPlayerItem(asset: asset)
         let player = AVPlayer(playerItem: playerItem)
-        
         let playerLayer = AVPlayerLayer(player: player)
+        
+        
+        playerLayer.frame = bounds
+        playerLayer.videoGravity = .resizeAspectFill
         let controller = AVPlayerViewController()
         controller.player = player
-        
-        playerLayer.frame = bound
-        playerLayer.videoGravity = .resizeAspect
-        theLayer.layer.addSublayer(playerLayer)
-        
-       
-            
-        //AVPlayer object
-            self.present(controller, animated: true) {[weak self] in
-               DispatchQueue.main.async {
+        self.present(controller, animated: true) {[weak self] in
+            DispatchQueue.main.async {
                 player.isMuted = true
-                 player.play()
-               }
-
+                player.play()
             }
-
-       
-            player.isMuted = true
-            player.play()
-       
-    
-        NotificationCenter.default.addObserver(self, selector: #selector(landscapeModePlayer), name: NSNotification.Name("landscapeMode"), object: nil)
+        }
         
-        NotificationCenter.default.addObserver(self, selector: #selector(potraitModePlayer), name: NSNotification.Name("portraitMode"), object: nil)
+        
+        player.isMuted = true
+        player.play()
+        
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(landscapeModePlayer), name: NSNotification.Name("landscapeMode"), object: nil)
+      
     }
-
-    @objc func landscapeModePlayer () {
+    
+    @objc func landscapeModePlayer (bound: CGRect, theLayer: UIView)  {
         print("landscape çalışıyor")
+        let videoURL = URL(string: theNewsDetail[0].videoUrl)
+        let asset = AVAsset(url: videoURL!)
+        let playerItem = AVPlayerItem(asset: asset)
+        let player = AVPlayer(playerItem: playerItem)
+        let controller = AVPlayerViewController()
+        controller.player = player
+ 
+        let playerLayer = AVPlayerLayer(player: player)
+        
+        playerLayer.videoGravity = .resizeAspect
+        playerLayer.frame = bound
+        theLayer.layer.addSublayer(playerLayer)
+        player.play()
+  
+        
+        
     }
-    @objc func potraitModePlayer () {
+    
+   
+    
+    @objc func portraitModePlayer (cellBound: CGRect) ->  CGRect {
+        
+        return cellBound
+        
+        
         print("portrait çalışıyor")
     }
-  
+    
     //VC Ends Here
 }
 
@@ -140,8 +161,9 @@ extension NewsDetailVC: UITableViewDataSource {
                 return cell
             } else {
                 print("Değilim", "Video")
-           
-                videoPlaying(bound: cell.topView.bounds, theLayer: cell.topView)
+             
+                landscapeModePlayer(bound: cell.topView.bounds, theLayer: cell.topView)
+              
                 
                 cell.selectionStyle = .none
                 return cell
